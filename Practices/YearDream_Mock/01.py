490886fe12144cdc47e57d6e09369d226175b67e
YI-Hongju <to_hongju@protonmail.com>
2022-06-30 20:37:08 +0900

    Lecturer issue, Similar quizs

---
+++ Practices/YearDream_Mock/01.py
@@ -0,0 +1,25 @@
+# def solution(h, k, boxes):
+#     answer = int()
+
+#     boxes.append(h)
+#     sorted(boxes)
+
+#     location = int()
+#     step = list()
+
+#     while location != h:
+        
+
+#     return boxes
+
+
+# h = 12
+# k = 3
+# boxes = [2, 3, 6, 7, 8, 10, 11]
+# solution(h, k, boxes)
+
+# 비슷한 문제
+{'문제 1': 'https://programmers.co.kr/learn/courses/30/lessons/42883?language=python3',
+ '문제 2': 'https://programmers.co.kr/learn/courses/30/lessons/42888?language=python3',
+ '문제 3': 'https://programmers.co.kr/learn/courses/30/lessons/42584?language=python3',
+ '문제 4': 'https://programmers.co.kr/learn/courses/30/lessons/42862?language=python3'}
