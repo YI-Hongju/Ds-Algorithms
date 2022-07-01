9e49e4fe8a9a48dc0ea2863e845e362f61e5c159
YI-Hongju <to_hongju@protonmail.com>
2022-07-01 17:25:47 +0900

    Handled scanning float issue, Ignore image files

--- Practices/MovieDB/src/db.c
+++ Practices/MovieDB/src/db.c
@@ -7,37 +7,20 @@ typedef struct {
     float rating;
 } SingleMovieInfo;
 
-// void clearIBuffer(void) {
-//     char c;
-
-//     // while ((c = getchar()) == '\n')
-//     //     continue;
-
-//     c = getchar();
-// }
-
 void dataBasing(FILE * fPtr) {
     unsigned char nItems;
     char dummy;
 
     fscanf(fPtr, "%hhu", &nItems);
-    // printf("%hhu\n", nItems);
     dummy = fgetc(fPtr);
 
     SingleMovieInfo movies[nItems];
 
     for (unsigned char i = 0; i < nItems; i++) {
         fgets(movies[i].title, N_MAX_MOVIE_TITLE, fPtr);
-        dummy = fgetc(fPtr);
 
         fscanf(fPtr, "%f", &movies[i].rating);
         dummy = fgetc(fPtr);
-    }
-
-    for (unsigned char i = 0; i < nItems; i++) {
-        printf("movies%hhu.title => %s", i, movies[i].title);
-
-        printf("movies%hhu.rating => %.2f\n", i, movies[i].rating);
     }
 
     fclose(fPtr);
